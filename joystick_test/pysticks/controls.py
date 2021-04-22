7bff052df7683662f5eb64bc8d83d01e6e2706a6
ArtimisFowl888 <fun1595@gmail.com>
2021-04-22 11:35:36 -0500

    finished server and client hopefully

--- joystick_test/pysticks/controls.py
+++ joystick_test/pysticks/controls.py
@@ -4,8 +4,11 @@ from sys import stdout
 import pygame
 
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '192.168.1.42'  # The server's hostname or IP address
 PORT = 21567  # The port used by the server
+
+# HOST = '127.0.0.1'  # The server's hostname or IP address
+# PORT = 65432
 
 # Initialize pygame for joystick support
 pygame.display.init()
@@ -13,25 +16,24 @@ pygame.joystick.init()
 controller = pygame.joystick.Joystick(0)
 controller.init()
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        # Get next pygame event
+        pygame.event.pump()
+        y = controller.get_axis(1)
+        x = controller.get_axis(0)
+        L = -y + x
+        R = -y - x
+        v = -controller.get_axis(3)
+        g1 = controller.get_button(0)
+        g2 = controller.get_button(1)
+        c5u = controller.get_button(4)
+        c5d = controller.get_button(2)
+        c1u = controller.get_button(5)
+        c1d = controller.get_button(3)
+        d = str('%2.2f,%2.2f,%2.2f,%x,%x,%x,%x,%x,%x,R' % (L, R, v, g1, g2, c5u, c5d, c1u, c1d))
 
-while True:
-    # Get next pygame event
-    pygame.event.pump()
-    y = controller.get_axis(1)
-    x = controller.get_axis(0)
-    L = -y + x
-    R = -y - x
-    v = -controller.get_axis(3)
-    g1 = controller.get_button(0)
-    g2 = controller.get_button(1)
-    c5u = controller.get_button(4)
-    c5d = controller.get_button(2)
-    c1u = controller.get_button(5)
-    c1d = controller.get_button(3)
-    d = str('%2.2f,%2.2f,%2.2f,%x,%x,%x,%x,%x,%x,R' % (L, R, v, g1, g1, c5u, c5d, c1u, c1d))
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
         s.send(d.encode("utf-8"))
         data = s.recv(1024).decode("utf-8")
         print('Received', repr(data))
-        s.close()
--- server/echo2.py
+++ server/echo2.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = '192.168.1.42'  # Standard loopback interface address (localhost)
 PORT = 21567  # Port to listen on (non-privileged ports are > 1023)
 scale = 90
 
@@ -10,30 +10,44 @@ V = 0
 cam = 100
 angle = 1000 + cam * 1000 / 180
 n = 0
+claw = 1000 + cam * 1000 / 180
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    while True:
 
+    def conecct():
+        global conn, addr
+        conn, addr = s.accept()
+        print('Connected by', addr)
+
+    while True:
+        global conn, addr
+        conecct()
         with conn:
-            print('Connected by', addr)
+
             while True:
+
                 try:
                     Dat = conn.recv(1024).decode("utf-8")
                     data = Dat.split(",")
+                except:
+                    conecct()
+
+                try:
                     if not Dat:
                         break
+                        conecct()
                     if data[9] == 'R':
                         print(data)
                         i = float(data[0])
                         j = float(data[1])
                         k = float(data[2])
 
-                        if (abs(i) < 0.2):
+                        if (abs(i) < 0.05):
                             i = 0
-                        if (abs(j) < 0.2):
+                        if (abs(j) < 0.05):
                             j = 0
                         if (abs(k) < 0.2):
                             k = 0
@@ -49,6 +63,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             angle = 1200
                         if (angle > 2000):
                             angle = 2000
+
+                        if int(data[3]) == 1:
+                            claw = claw + 0.15
+                        if int(data[4]) == 1:
+                            claw = claw - 0.15
+                        if (claw < 1200):
+                            claw = 1200
+                        if (claw > 2000):
+                            claw = 2000
                         # pi.set_servo_pulsewidth(23,angle)
                         #
                         #
@@ -57,7 +80,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         # c.LV(V*scale)
                         # c.RV(V*scale)
 
-                        data2 = str('%2.2f,%2.2f,%2.2f,%2.2f' % (L * scale, R * scale, V * scale, angle))
+                        data2 = str('%2.2f,%2.2f,%2.2f,%2.2f,%2.2f' % (L * scale, R * scale, V * scale, angle, claw))
                         conn.sendall(data2.encode("utf-8"))
                 except:
                     continue
